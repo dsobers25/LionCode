f66189a375b27ac6d03de38c4bb732e8c506a3a9
fgdavis <33137464+fgdavis@users.noreply.github.com>
2018-10-20 09:58:24 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,30 +1,24 @@
 package com.example.lioncode.sec;
 
 public class Student {
-    private int _id;
-    private String _studentname;
+    private String _email;
     private String _password;
     private String _clockin;
     private String _clockout;
 
     public Student() {
     }
-    public Student(int id, String studentname, String password) {
-        this._id = id;
-        this._studentname = studentname;
+    public Student(String email, String password, String clockin, String clockout) {
+        this._email = email;
         this._password = password;
+        this._clockin = clockin;
+        this._clockout = clockout;
     }
-    public void setID(int id) {
-        this._id = id;
+    public void setEmail(String email) {
+        this._email = email;
     }
-    public int getID() {
-        return this._id;
-    }
-    public void setStudentName(String studentname) {
-        this._studentname = studentname;
-    }
-    public String getStudentName() {
-        return this._studentname;
+    public String getEmail() {
+        return this._email;
     }
     public void setPassword(String password){
         this._password = password;
